a2aed1cb15d21adb9629d0187f60e73c0567f59e
jvn1o <junok0987@naver.com>
2025-01-20 18:37:00 +0900

    Day2

---
+++ Day2_1.java
@@ -0,0 +1,12 @@
+/*
+    정수 배열 numbers가 매개변수로 주어집니다.
+    numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+*/
+
+import java.util.Arrays;
+
+public class Day2_1 {
+    public double solution(int[] numbers) {
+        return Arrays.stream(numbers).average().orElse(0);
+    }
+}
---
+++ Day2_3.java
@@ -0,0 +1,16 @@
+/*
+    머쓱이네 양꼬치 가게는 10인분을 먹으면 음료수 하나를 서비스로 줍니다.
+    양꼬치는 1인분에 12,000원, 음료수는 2,000원입니다.
+    정수 n과 k가 매개변수로 주어졌을 때, 양꼬치 n인분과 음료수 k개를 먹었다면
+    총얼마를 지불해야 하는지 return 하도록 solution 함수를 완성해보세요.
+*/
+
+public class Day2_3 {
+    public int solution(int n, int k) {
+        int lamb = (n * 12000);
+        int service = (n / 10);
+        int drink = (k - service) * 2000;
+        int sum = lamb + drink;
+        return sum;
+    }
+}
