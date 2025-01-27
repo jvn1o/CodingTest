3b8b99f77f80e914c7a3a2105b279d50811ba6e3
jvn1o <junok0987@naver.com>
2025-01-27 20:12:10 +0900

    Day9

---
+++ Day9_1.java
@@ -0,0 +1,17 @@
+/*
+    정수 n이 매개변수로 주어질 때 n의
+    각 자리 숫자의 합을 return 하도록 solution 함수를 완성해주세요
+*/
+
+public class Day9_1 {
+    public int solution(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
