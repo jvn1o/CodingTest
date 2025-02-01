b134e23753dcfc09e5afb4692e8c9157aaa5a763
jvn1o <junok0987@naver.com>
2025-02-01 21:47:28 +0900

    Day14

--- Day14_2.java
+++ Day14_2.java
@@ -7,21 +7,12 @@
 import java.util.*;
 
 public class Day14_2 {
-    public int[] solution(String my_string) {
-        ArrayList<Integer> numberList = new ArrayList<>();
+    public int solution(String my_string) {
+        int answer = 0;
 
         for (char c : my_string.toCharArray()) {
-            if (Character.isDigit(c)) {
-                numberList.add(Character.getNumericValue(c));
-            }
-        }
-
-        Collections.sort(numberList);
-
-        int[] answer = new int[numberList.size()];
-
-        for (int i = 0; i < numberList.size(); i++) {
-            answer[i] = numberList.get(i);
+            if (Character.isDigit(c))
+                answer += Character.getNumericValue(c);
         }
 
         return answer;
---
+++ Day14_3.java
@@ -0,0 +1,8 @@
+/*
+    문자열 my_string 이 매개변수로 주어집니다.
+    my_string 안의 모든 자연수들의 합을 return 하도록 solution 함수를 완성해주세요.
+*/
+
+public class Day14_3 {
+
+}
