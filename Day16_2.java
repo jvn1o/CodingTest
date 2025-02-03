dff1db9668d507e8cf50cc488d2c4d13fe5852d2
jvn1o <junok0987@naver.com>
2025-02-03 23:03:42 +0900

    Day16

--- Day16_2.java
+++ Day16_2.java
@@ -10,7 +10,7 @@ public class Day16_2 {
 
         for (int i = 0; i < array.length - 1; i++) {
             if (Math.abs(array[i] - n) <
-                    Math.abs(array[i + 1]))
+                    Math.abs(array[i + 1] - n))
                 answer = array[i];
             else answer = array[i + 1];
         }
