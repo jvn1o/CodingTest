cb1ee25fd25da987f123187a392a800e1cdc4374
jvn1o <junok0987@naver.com>
2025-02-04 13:42:29 +0900

    Day17

---
+++ Day17_1.java
@@ -0,0 +1,13 @@
+/*
+    문자열 str 이 주어질 때, str 을 출력하는 코드를 작성해 보세요.
+*/
+
+import java.util.Scanner;
+
+public class Day17_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.printf(a);
+    }
+}
