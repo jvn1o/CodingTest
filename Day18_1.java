07da65679c4ad5b6da9c9c388c2797b04961c89b
jvn1o <junok0987@naver.com>
2025-02-05 15:06:29 +0900

    Day18

--- Day18_1.java
+++ Day18_1.java
@@ -10,8 +10,8 @@ public class Day18_1 {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
 
-        for (int i = 0; i < a.length(); i++) {
-            System.out.println(a.charAt(i));
+        for (char ch : a.toCharArray()) {
+            System.out.println(ch);
         }
     }
 }
