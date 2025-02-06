38f9d9b3da393ca62db31cf8168c96dcaa0e79c4
jvn1o <junok0987@naver.com>
2025-02-06 13:49:55 +0900

    Day19

--- Day19_1.java
+++ Day19_1.java
@@ -12,6 +12,9 @@
 
 public class Day19_1 {
     public int solution(int a, int b) {
-        return Integer.parseInt(a + "" + b);
+        int ab = Integer.parseInt(a + "" + b);
+        int ba = Integer.parseInt(b + "" + a);
+
+        return Math.max(ab, ba);
     }
 }
