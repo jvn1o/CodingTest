697bf1a3edc6b217039bb07c8089148fb2b09306
jvn1o <junok0987@naver.com>
2025-02-08 16:11:10 +0900

    Day21

---
+++ Day21_3.java
@@ -0,0 +1,19 @@
+/*
+    정수가 담긴 리스트 num_list 가 주어질 때,
+    모든 원소들의 곱이 모든 원소들의 합의 제곱보다 작으면 1을 크면 0을 return 하도록
+    solution 함수를 완성해주세요.
+*/
+
+class Day21_3 {
+    public int solution(int[] num_list) {
+        int sum = 0;
+        int multiple = 1;
+
+        for (Integer ch : num_list) {
+            sum += ch;
+            multiple *= ch;
+        }
+
+        return multiple < (int) Math.pow(sum,2) ? 1 : 0;
+    }
+}
