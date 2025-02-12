149070df8d5297ad077088361a39aa8cca687865
jvn1o <junok0987@naver.com>
2025-02-12 22:34:01 +0900

    Day25

--- Day25_2.java
+++ Day25_2.java
@@ -18,19 +18,23 @@ import java.util.ArrayList;
 class Day25_2 {
     public int[] solution(int[] arr) {
         ArrayList<Integer> stk = new ArrayList<>();
+        int i = 0;
 
-        for (int j : arr) {
-            if (stk.isEmpty() || stk.get(stk.size() - 1) < j) {
-                stk.add(j);
+        while (i < arr.length) {
+            if (stk.isEmpty()) {
+                stk.add(arr[i]);
+                i++;
+            } else if (stk.get(stk.size() - 1) < arr[i]) {
+                stk.add(arr[i]);
+                i++;
             } else {
                 stk.remove(stk.size() - 1);
-                stk.add(j);
             }
         }
 
         int[] answer = new int[stk.size()];
-        for (int i = 0; i < stk.size(); i++) {
-            answer[i] = stk.get(i);
+        for (int j = 0; j < stk.size(); j++) {
+            answer[j] = stk.get(j);
         }
 
         return answer;
