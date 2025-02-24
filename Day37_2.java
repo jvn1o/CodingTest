343d99506b89c6a075b0b7e7f52e869fa1954b6a
jvn1o <junok0987@naver.com>
2025-02-24 14:10:57 +0900

    Day37

---
+++ Day37_2.java
@@ -0,0 +1,24 @@
+/*
+    문자열 binomial 이 매개변수로 주어집니다.
+    binomial 은 "a op b" 형태의 이항식이고 a와 b는 음이 아닌 정수,
+    op는 '+', '-', '*' 중 하나입니다.
+    주어진 식을 계산한 정수를 return 하는 solution 함수를 작성해 주세요.
+*/
+
+import java.util.Arrays;
+
+class Day37_2 {
+    public int solution(String binomial) {
+        String[] bio = Arrays.stream(binomial.split(" ")).toArray(String[]::new);
+        int a = Integer.parseInt(bio[0]);
+        String op = bio[1];
+        int b = Integer.parseInt(bio[2]);
+
+        return switch (op) {
+            case "+" -> a + b;
+            case "-" -> a - b;
+            case "*" -> a * b;
+            default -> 0;
+        };
+    }
+}
