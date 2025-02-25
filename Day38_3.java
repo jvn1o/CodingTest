cbe4abded9c3934bab7e9f4534e68115a22bf4d2
jvn1o <junok0987@naver.com>
2025-02-25 20:14:01 +0900

    Day38

--- Day38_3.java
+++ Day38_3.java
@@ -21,20 +21,20 @@ import java.util.Set;
 class Day38_3 {
     public int[] solution(int[] arr, int k) {
         Set<Integer> set = new HashSet<>();
-        List<Integer> list = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
 
         for (int num : arr) {
             if (!set.contains(num)) {
                 set.add(num);
-                list.add(num);
-                if (list.size() == k) break;
+                answer.add(num);
+                if (answer.size() == k) break;
             }
         }
 
-        while (list.size() < k) {
-            list.add(-1);
+        while (answer.size() < k) {
+            answer.add(-1);
         }
-        
-        return answer;
+
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
