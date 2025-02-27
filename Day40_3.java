49d7e1a0315937ca68c8324a2eaf383ba76b2c6f
jvn1o <junok0987@naver.com>
2025-02-27 17:27:25 +0900

    Day40

--- Day40_3.java
+++ Day40_3.java
@@ -4,9 +4,19 @@
     solution 함수를 완성해주세요.
 */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Day40_3 {
     public int[] solution(int[] num_list) {
-        int[] answer = {};
-        return answer;
+        Arrays.sort(num_list);
+
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 5; i < num_list.length; i++) {
+            answer.add(num_list[i]);
+        }
+
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
