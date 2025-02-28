9aa1ad1203514ef3f6a53019eb687e29dcbe6fe9
jvn1o <junok0987@naver.com>
2025-02-28 18:47:52 +0900

    Day41

---
+++ Day41_2.java
@@ -0,0 +1,11 @@
+/*
+    실수 flo 가 매개 변수로 주어질 때,
+    flo 의 정수 부분을 return 하도록
+    solution 함수를 완성해주세요.
+*/
+
+class Day41_2 {
+    public int solution(double flo) {
+        return (int) flo;
+    }
+}
