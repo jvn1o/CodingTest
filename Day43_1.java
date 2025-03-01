e443af92eba6929a93c61e095c361da89659a746
jvn1o <junok0987@naver.com>
2025-03-02 03:58:50 +0900

    Day43

--- Day43_1.java
+++ Day43_1.java
@@ -11,7 +11,6 @@
 
 class Day43_1 {
     public int solution(String my_string, String target) {
-        int answer = 0;
-        return answer;
+        return my_string.contains(target) ? 1 : 0;
     }
 }
