41382529ac75995b6dc7829e191e2c694560abbd
jvn1o <junok0987@naver.com>
2025-03-02 03:58:02 +0900

    Day43

---
+++ Day43_1.java
@@ -0,0 +1,17 @@
+/*
+    부분 문자열이란 문자열에서 연속된 일부분에 해당하는 문자열을 의미합니다.
+    예를 들어, 문자열 "ana", "ban", "anana", "banana", "n"는
+    모두 문자열 "banana"의 부분 문자열이지만,
+    "aaa", "bnana", "wxyz"는 모두 "banana"의 부분 문자열이 아닙니다.
+
+    문자열 my_string 과 target 이 매개변수로 주어질 때,
+    target 이 문자열 my_string 의 부분 문자열이라면 1을,
+    아니라면 0을 return 하는 solution 함수를 작성해 주세요.
+*/
+
+class Day43_1 {
+    public int solution(String my_string, String target) {
+        int answer = 0;
+        return answer;
+    }
+}
