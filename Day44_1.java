43459daeee5a105a9de4ad2888bfd65068c1a161
jvn1o <junok0987@naver.com>
2025-03-03 23:40:57 +0900

    Day44

--- Day44_1.java
+++ Day44_1.java
@@ -9,7 +9,11 @@
 
 class Day44_1 {
     public int solution(int[] date1, int[] date2) {
-        int answer = 0;
-        return answer;
+
+        for (int i = 0; i < 3; i++) {
+            if (date1[i] < date2[i]) return 1;
+            if (date1[i] > date2[i]) return 0;
+        }
+        return 0;
     }
 }
