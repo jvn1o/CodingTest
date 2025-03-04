c3d7e8e87536aabb6573e989ee2799c33548fe1f
jvn1o <junok0987@naver.com>
2025-03-04 20:06:15 +0900

    Day45

--- Day45_1.java
+++ Day45_1.java
@@ -7,14 +7,13 @@
     아니면 0을 return 하는 solution 함수를 완성해 주세요.
 */
 
+import java.time.LocalDate;
+
 class Day45_1 {
     public int solution(int[] date1, int[] date2) {
-        if (date1[0] != date2[0]) {
-            return date1[0] < date2[0] ? 1 : 0;
-        }
-        if (date1[1] != date2[1]) {
-            return date1[1] < date2[1] ? 1 : 0;
-        }
-        return date1[2] < date2[2] ? 1 : 0;
+        LocalDate dateA = LocalDate.of(date1[0], date1[1], date1[2]);
+        LocalDate dateB = LocalDate.of(date2[0], date2[1], date2[2]);
+
+        return dateA.isBefore(dateB) ? 1 : 0;
     }
 }
