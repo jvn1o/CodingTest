c8ab3e4213af45aa3bcbb56eb0ec34c114cb6659
jvn1o <junok0987@naver.com>
2025-03-07 16:28:11 +0900

    Day48

--- Day48_2.java
+++ Day48_2.java
@@ -17,9 +17,32 @@
     solution 함수를 작성해주세요.
 */
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Day48_2 {
     public int[] solution(int[] answers) {
-        int[] answer = {};
-        return answer;
+        int[] p1 = {1, 2, 3, 4, 5};           // 1번 수포자 5개 패턴 반복
+        int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};  // 2번 수포자 8개 패턴 반복
+        int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; // 3번 수포자 10개 패턴 반복
+
+        int[] scores = new int[3]; // {1번, 2번, 3번}의 맞힌 개수
+
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == p1[i % p1.length]) scores[0]++;
+            if (answers[i] == p2[i % p2.length]) scores[1]++;
+            if (answers[i] == p3[i % p3.length]) scores[2]++;
+        }
+
+        int maxScore = Math.max(scores[0], Math.max(scores[1], scores[2]));
+
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            if (scores[i] == maxScore) {
+                answer.add(i + 1);
+            }
+        }
+
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
