55febaf80fec75c1cefb71ee0304b336767e15a2
jvn1o <junok0987@naver.com>
2025-03-08 22:38:05 +0900

    Day49

--- Day49_1.java
+++ Day49_1.java
@@ -6,9 +6,37 @@
     종이 조각으로 만들 수 있는 소수가 몇 개인지 return 하도록 solution 함수를 완성해주세요.
 */
 
+import java.util.HashSet;
+import java.util.Set;
+
 class Day49_1 {
     public int solution(String numbers) {
-        int answer = 0;
-        return answer;
+        Set<Integer> numberSet = new HashSet<>();
+        int length = numbers.length();
+
+        // 모든 순열 생성 (비트마스크를 사용하여 조합을 만듦)
+        boolean[] visited = new boolean[length];
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= length; i++) { // i는 생성할 숫자의 길이
+            permute(numbers, visited, sb, numberSet, i);
+        }
+
+        // 소수 개수 세기
+        int count = 0;
+        for (int num : numberSet) {
+            if (num < 2) continue;
+            boolean isPrime = true;
+            for (int j = 2; j * j <= num; j++) {
+                if (num % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) count++;
+        }
+
+        return count;
+    }
     }
 }
