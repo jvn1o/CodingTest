37ab06ae6428be056420d3468c88442d02c13a08
jvn1o <junok0987@naver.com>
2025-03-09 01:35:10 +0900

    Day50

--- Day50_1.java
+++ Day50_1.java
@@ -11,9 +11,43 @@
 */
 
 
+import java.util.Arrays;
+
 class Day50_1 {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-        return answer;
+        int[] students = new int[n + 1];
+
+        Arrays.fill(students, 1);
+
+        for (int l : lost) {
+            students[l]--;
+        }
+
+        for (int r : reserve) {
+            students[r]++;
+        }
+
+        // 체육복 빌려주기 (앞뒤 학생 확인)
+        for (int i = 1; i <= n; i++) {
+            if (students[i] == 0) { // 체육복이 없는 경우
+                if (i > 1 && students[i - 1] == 2) { // 앞 학생이 여벌이 있으면 빌리기
+                    students[i - 1]--;
+                    students[i]++;
+                } else if (i < n && students[i + 1] == 2) { // 뒷 학생이 여벌이 있으면 빌리기
+                    students[i + 1]--;
+                    students[i]++;
+                }
+            }
+        }
+
+        // 체육복이 1개 이상인 학생 수 계산
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (students[i] >= 1) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
