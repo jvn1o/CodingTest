588ec16879f71dd6956b743cf2a1a6ac505d0d33
jvn1o <junok0987@naver.com>
2025-03-17 21:40:47 +0900

    Day51

--- Algorithm/Day51_2.java
+++ Algorithm/Day51_2.java
@@ -8,8 +8,7 @@ class Day51_2 {
     public int solution(int[] numbers) {
         int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE;
 
-        int min1 = Integer.MAX_VALUE;
-        int min2 = Integer.MAX_VALUE;
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
 
         for (int num : numbers) {
             if (num > max1) {
