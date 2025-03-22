c08ef72967e85ae9d94f82d3c8b5069b87fe531b
jvn1o <junok0987@naver.com>
2025-03-23 00:49:02 +0900

    Day6

--- SQL/Day6_4.java
+++ SQL/Day6_4.java
@@ -1,37 +1,27 @@
 /*
-    다음은 아이스크림 가게의 상반기 주문 정보를 담은 FIRST_HALF 테이블과
-    아이스크림 성분에 대한 정보를 담은 ICECREAM_INFO 테이블입니다.
-    FIRST_HALF 테이블 구조는 다음과 같으며,
-    SHIPMENT_ID, FLAVOR, TOTAL_ORDER 는 각각
-    아이스크림 공장에서 아이스크림 가게까지의 출하 번호, 아이스크림 맛, 상반기 아이스크림 총주문량을 나타냅니다. FIRST_HALF 테이블의 기본 키는 FLAVOR입니다.
+    ANIMAL_INS 테이블은 동물 보호소에 들어온 동물의 정보를 담은 테이블입니다.
+    ANIMAL_INS 테이블 구조는 다음과 같으며,
+    ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE 는 각각
+    동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.
 
     NAME	TYPE	NULLABLE
-    SHIPMENT_ID	INT(N)	FALSE
-    FLAVOR	VARCHAR(N)	FALSE
-    TOTAL_ORDER	INT(N)	FALSE
+    ANIMAL_ID	VARCHAR(N)	FALSE
+    ANIMAL_TYPE	VARCHAR(N)	FALSE
+    DATETIME	DATETIME	FALSE
+    INTAKE_CONDITION	VARCHAR(N)	FALSE
+    NAME	VARCHAR(N)	TRUE
+    SEX_UPON_INTAKE	VARCHAR(N)	FALSE
 
-    ICECREAM_INFO 테이블 구조는 다음과 같으며,
-    FLAVOR, INGREDITENT_TYPE 은 각각 아이스크림 맛, 아이스크림의 성분 타입을 나타냅니다.
-    INGREDIENT_TYPE에는 아이스크림의 주 성분이 설탕이면 sugar_based라고 입력되고,
-    아이스크림의 주 성분이 과일이면 fruit_based라고 입력됩니다.
-    ICECREAM_INFO의 기본 키는 FLAVOR입니다.
-    ICECREAM_INFO테이블의 FLAVOR는 FIRST_HALF 테이블의 FLAVOR의 외래 키입니다.
-
-    NAME	TYPE	NULLABLE
-    FLAVOR	VARCHAR(N)	FALSE
-    INGREDIENT_TYPE	VARCHAR(N)	FALSE
-
-    상반기 동안 각 아이스크림 성분 타입과 성분 타입에 대한 아이스크림의 총주문량을
-    총주문량이 작은 순서대로 조회하는 SQL 문을 작성해주세요.
-    이때 총주문량을 나타내는 컬럼명은 TOTAL_ORDER로 지정해주세요.
+    동물 보호소에 들어온 동물 중 고양이와 개가 각각 몇 마리인지 조회하는 SQL 문을 작성해주세요.
+    이때 고양이를 개보다 먼저 조회해주세요.
 */
 
 /*
     SELECT
-        I.INGREDIENT_TYPE,
-        SUM(F.TOTAL_ORDER) AS "TOTAL_ORDER"
-    FROM ICECREAM_INFO I
-    JOIN FIRST_HALF F ON I.FLAVOR = F.FLAVOR
-    GROUP BY I.INGREDIENT_TYPE
-    ORDER BY TOTAL_ORDER ASC;
+        ANIMAL_TYPE,
+        COUNT(*) AS "count"
+    FROM ANIMAL_INS
+    WHERE ANIMAL_TYPE = "Cat" OR ANIMAL_TYPE = "Dog"
+    GROUP BY ANIMAL_TYPE
+    ORDER BY ANIMAL_TYPE ASC;
 */
