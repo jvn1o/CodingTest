28b6ad719c8ecebbfa90ff2465a1f52783553f72
jvn1o <junok0987@naver.com>
2025-03-27 23:15:45 +0900

    Day10

--- SQL/Day10_2.java
+++ SQL/Day10_2.java
@@ -1,6 +1,6 @@
 /*
     ANIMAL_INS 테이블은 동물 보호소에 들어온 동물의 정보를 담은 테이블입니다.
-    ANIMAL_INS 테이블 구조는 다음과 같으며, 
+    ANIMAL_INS 테이블 구조는 다음과 같으며,
     ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE 는 각각
     동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.
 
@@ -16,29 +16,8 @@
 */
 
 /*
-    SELECT
-        SALES_DATE,
-        PRODUCT_ID,
-        USER_ID,
-        SALES_AMOUNT
-    FROM (
-        SELECT
-            TO_CHAR(SALES_DATE, 'YYYY-MM-DD') SALES_DATE,
-            PRODUCT_ID,
-            USER_ID,
-            SALES_AMOUNT
-        FROM ONLINE_SALE
-        WHERE TO_CHAR(SALES_DATE, 'YYYY-MM') = '2022-03'
-
-        UNION ALL
-
-        SELECT
-            TO_CHAR(SALES_DATE, 'YYYY-MM-DD') SALES_DATE,
-            PRODUCT_ID,
-            NULL AS USER_ID,
-            SALES_AMOUNT
-        FROM OFFLINE_SALE
-        WHERE TO_CHAR(SALES_DATE, 'YYYY-MM') = '2022-03'
-    ) SALES
-    ORDER BY SALES_DATE, PRODUCT_ID, USER_ID;
+    SELECT DATETIME
+    FROM ANIMAL_INS
+    ORDER BY DATETIME ASC
+    FETCH FIRST 1 ROWS ONLY;
 */
