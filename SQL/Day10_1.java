826cd42b2de0ebf1898231f764eeaf8ba3bf2d21
jvn1o <junok0987@naver.com>
2025-03-27 22:57:55 +0900

    Day10

--- SQL/Day10_1.java
+++ SQL/Day10_1.java
@@ -1,18 +1,37 @@
 /*
-    다음은 어느 한 서점에서 판매중인 도서들의 도서 정보(BOOK) 테이블입니다.
+    다음은 어느 의류 쇼핑몰의 온라인 상품 판매 정보를 담은 ONLINE_SALE 테이블과
+    오프라인 상품 판매 정보를 담은 OFFLINE_SALE 테이블 입니다.
+    ONLINE_SALE 테이블은 아래와 같은 구조로 되어있으며
+    ONLINE_SALE_ID, USER_ID, PRODUCT_ID, SALES_AMOUNT, SALES_DATE 는 각각
+    온라인 상품 판매 ID, 회원 ID, 상품 ID, 판매량, 판매일을 나타냅니다.
 
-    BOOK 테이블은 각 도서의 정보를 담은 테이블로 아래와 같은 구조로 되어있습니다.
+    Column name	Type	Nullable
+    ONLINE_SALE_ID	INTEGER	FALSE
+    USER_ID	INTEGER	FALSE
+    PRODUCT_ID	INTEGER	FALSE
+    SALES_AMOUNT	INTEGER	FALSE
+    SALES_DATE	DATE	FALSE
 
-    Column name	Type	Nullable	Description
-    BOOK_ID	INTEGER	FALSE	도서 ID
-    CATEGORY	VARCHAR(N)	FALSE	카테고리 (경제, 인문, 소설, 생활, 기술)
-    AUTHOR_ID	INTEGER	FALSE	저자 ID
-    PRICE	INTEGER	FALSE	판매가 (원)
-    PUBLISHED_DATE	DATE	FALSE	출판일
+    동일한 날짜, 회원 ID, 상품 ID 조합에 대해서는 하나의 판매 데이터만 존재합니다.
 
-    BOOK 테이블에서 2021년에 출판된 '인문' 카테고리에 속하는 도서 리스트를 찾아서
-    도서 ID(BOOK_ID), 출판일 (PUBLISHED_DATE)을 출력하는 SQL 문을 작성해주세요.
-    결과는 출판일을 기준으로 오름차순 정렬해주세요.
+    OFFLINE_SALE 테이블은 아래와 같은 구조로 되어있으며
+    OFFLINE_SALE_ID, PRODUCT_ID, SALES_AMOUNT, SALES_DATE 는 각각
+    오프라인 상품 판매 ID, 상품 ID, 판매량, 판매일을 나타냅니다.
+
+    Column name	Type	Nullable
+    OFFLINE_SALE_ID	INTEGER	FALSE
+    PRODUCT_ID	INTEGER	FALSE
+    SALES_AMOUNT	INTEGER	FALSE
+    SALES_DATE	DATE	FALSE
+    동일한 날짜, 상품 ID 조합에 대해서는 하나의 판매 데이터만 존재합니다.
+
+    ONLINE_SALE 테이블과 OFFLINE_SALE 테이블에서
+    2022년 3월의 오프라인/온라인 상품 판매 데이터의
+    판매 날짜, 상품 ID, 유저 ID, 판매량을 출력하는 SQL 문을 작성해주세요.
+
+    OFFLINE_SALE 테이블의 판매 데이터의 USER_ID 값은 NULL 로 표시해주세요.
+    결과는 판매일을 기준으로 오름차순 정렬해주시고 판매일이 같다면 상품 ID를 기준으로 오름차순,
+    상품 ID 까지 같다면 유저 ID를 기준으로 오름차순 정렬해주세요.
 */
 
 /*
