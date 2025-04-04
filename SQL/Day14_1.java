034289483f72f8256c5d9c90e7ba42def16ed9e2
jvn1o <junok0987@naver.com>
2025-04-04 22:29:51 +0900

    Day14

--- SQL/Day14_1.java
+++ SQL/Day14_1.java
@@ -1,35 +1,55 @@
 /*
-    다음은 식당의 정보를 담은 REST_INFO 테이블입니다. REST_INFO 테이블은 다음과 같으며
-    REST_ID, REST_NAME, FOOD_TYPE, VIEWS, FAVORITES, PARKING_LOT, ADDRESS, TEL 은
-    식당 ID, 식당 이름, 음식 종류, 조회수, 즐겨찾기수, 주차장 유무, 주소, 전화번호를 의미합니다.
+    다음은 중고 거래 게시판 정보를 담은 USED_GOODS_BOARD 테이블과
+    중고 거래 게시판 사용자 정보를 담은 USED_GOODS_USER 테이블입니다.
+
+    USED_GOODS_BOARD 테이블은 다음과 같으며
+    BOARD_ID, WRITER_ID, TITLE, CONTENTS, PRICE, CREATED_DATE, STATUS, VIEWS 는
+    게시글 ID, 작성자 ID, 게시글 제목, 게시글 내용, 가격, 작성일, 거래상태, 조회수를 의미합니다.
 
     Column name	Type	Nullable
-    REST_ID	VARCHAR(5)	FALSE
-    REST_NAME	VARCHAR(50)	FALSE
-    FOOD_TYPE	VARCHAR(20)	TRUE
-    VIEWS	NUMBER	TRUE
-    FAVORITES	NUMBER	TRUE
-    PARKING_LOT	VARCHAR(1)	TRUE
-    ADDRESS	VARCHAR(100)	TRUE
-    TEL	VARCHAR(100)	TRUE
+    BOARD_ID	VARCHAR(5)	FALSE
+    WRITER_ID	VARCHAR(50)	FALSE
+    TITLE	VARCHAR(100)	FALSE
+    CONTENTS	VARCHAR(1000)	FALSE
+    PRICE	NUMBER	FALSE
+    CREATED_DATE	DATE	FALSE
+    STATUS	VARCHAR(10)	FALSE
+    VIEWS	NUMBER	FALSE
 
-    REST_INFO 테이블에서 음식종류별로 즐겨찾기수가 가장 많은 식당의
-    음식 종류, ID, 식당 이름, 즐겨찾기수를 조회하는 SQL 문을 작성해주세요.
-    이때 결과는 음식 종류를 기준으로 내림차순 정렬해주세요.
+    USED_GOODS_USER 테이블은 다음과 같으며
+    USER_ID, NICKNAME, CITY, STREET_ADDRESS1, STREET_ADDRESS2, TLNO 는 각각
+    회원 ID, 닉네임, 시, 도로명 주소, 상세 주소, 전화번호를 를 의미합니다.
+
+    Column name	Type	Nullable
+    USER_ID	VARCHAR(50)	FALSE
+    NICKNAME	VARCHAR(100)	FALSE
+    CITY	VARCHAR(100)	FALSE
+    STREET_ADDRESS1	VARCHAR(100)	FALSE
+    STREET_ADDRESS2	VARCHAR(100)	TRUE
+    TLNO	VARCHAR(20)	FALSE
+
+    USED_GOODS_BOARD 와 USED_GOODS_USER 테이블에서 완료된 중고 거래의 총금액이 70만 원 이상인
+    사람의 회원 ID, 닉네임, 총거래금액을 조회하는 SQL 문을 작성해주세요.
+    결과는 총거래금액을 기준으로 오름차순 정렬해주세요.
 */
 
 /*
     SELECT
-        R.FOOD_TYPE,
-        R.REST_ID,
-        R.REST_NAME,
-        R.FAVORITES
+        U.USER_ID,
+        U.NICKNAME,
+        SUM(B.PRICE) AS TOTAL_SALES
     FROM
-        REST_INFO R
-    JOIN (
-        SELECT FOOD_TYPE, MAX(FAVORITES) AS FAVORITES
-        FROM REST_INFO
-        GROUP BY FOOD_TYPE
-    ) M ON R.FOOD_TYPE = M.FOOD_TYPE AND R.FAVORITES = M.FAVORITES
-    ORDER BY FOOD_TYPE DESC;
+        USED_GOODS_USER U
+    JOIN
+        USED_GOODS_BOARD B
+        ON U.USER_ID = B.WRITER_ID
+    WHERE
+        B.STATUS = 'DONE'
+    GROUP BY
+        U.USER_ID,
+        U.NICKNAME
+    HAVING
+        SUM(B.PRICE) >= 700000
+    ORDER BY
+        TOTAL_SALES ASC;
 */
