74b2e855dbeba39a4934ecf0d39705f455ed0b68
jvn1o <junok0987@naver.com>
2025-04-05 23:45:36 +0900

    Day15

--- SQL/Day15_1.java
+++ SQL/Day15_1.java
@@ -21,10 +21,10 @@
 /*
     SELECT
         ANIMAL_TYPE,
-        NAME,
+        NVL(NAME, 'No name') AS NAME,
         SEX_UPON_INTAKE
     FROM
         ANIMAL_INS
     ORDER BY
-        ANIMAL_TYPE;
+        ANIMAL_ID;
 */
