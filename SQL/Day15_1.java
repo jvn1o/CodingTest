58d92d8a889b72de6e328c683dde418d9fd3eae9
jvn1o <junok0987@naver.com>
2025-04-05 23:44:13 +0900

    Day15

--- SQL/Day15_1.java
+++ SQL/Day15_1.java
@@ -1,55 +1,30 @@
 /*
-    다음은 중고 거래 게시판 정보를 담은 USED_GOODS_BOARD 테이블과
-    중고 거래 게시판 사용자 정보를 담은 USED_GOODS_USER 테이블입니다.
+    ANIMAL_INS 테이블은 동물 보호소에 들어온 동물의 정보를 담은 테이블입니다.
+    ANIMAL_INS 테이블 구조는 다음과 같으며,
+    ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE 는 각각
+    동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.
 
-    USED_GOODS_BOARD 테이블은 다음과 같으며
-    BOARD_ID, WRITER_ID, TITLE, CONTENTS, PRICE, CREATED_DATE, STATUS, VIEWS 는
-    게시글 ID, 작성자 ID, 게시글 제목, 게시글 내용, 가격, 작성일, 거래상태, 조회수를 의미합니다.
+    NAME	TYPE	NULLABLE
+    ANIMAL_ID	VARCHAR(N)	FALSE
+    ANIMAL_TYPE	VARCHAR(N)	FALSE
+    DATETIME	DATETIME	FALSE
+    INTAKE_CONDITION	VARCHAR(N)	FALSE
+    NAME	VARCHAR(N)	TRUE
+    SEX_UPON_INTAKE	VARCHAR(N)	FALSE
 
-    Column name	Type	Nullable
-    BOARD_ID	VARCHAR(5)	FALSE
-    WRITER_ID	VARCHAR(50)	FALSE
-    TITLE	VARCHAR(100)	FALSE
-    CONTENTS	VARCHAR(1000)	FALSE
-    PRICE	NUMBER	FALSE
-    CREATED_DATE	DATE	FALSE
-    STATUS	VARCHAR(10)	FALSE
-    VIEWS	NUMBER	FALSE
-
-    USED_GOODS_USER 테이블은 다음과 같으며
-    USER_ID, NICKNAME, CITY, STREET_ADDRESS1, STREET_ADDRESS2, TLNO 는 각각
-    회원 ID, 닉네임, 시, 도로명 주소, 상세 주소, 전화번호를 를 의미합니다.
-
-    Column name	Type	Nullable
-    USER_ID	VARCHAR(50)	FALSE
-    NICKNAME	VARCHAR(100)	FALSE
-    CITY	VARCHAR(100)	FALSE
-    STREET_ADDRESS1	VARCHAR(100)	FALSE
-    STREET_ADDRESS2	VARCHAR(100)	TRUE
-    TLNO	VARCHAR(20)	FALSE
-
-    USED_GOODS_BOARD 와 USED_GOODS_USER 테이블에서 완료된 중고 거래의 총금액이 70만 원 이상인
-    사람의 회원 ID, 닉네임, 총거래금액을 조회하는 SQL 문을 작성해주세요.
-    결과는 총거래금액을 기준으로 오름차순 정렬해주세요.
+    입양 게시판에 동물 정보를 게시하려 합니다.
+    동물의 생물 종, 이름, 성별 및 중성화 여부를 아이디 순으로 조회하는 SQL 문을 작성해주세요.
+    이때 프로그래밍을 모르는 사람들은 NULL 이라는 기호를 모르기 때문에,
+    이름이 없는 동물의 이름은 "No name"으로 표시해 주세요.
 */
 
 /*
     SELECT
-        U.USER_ID,
-        U.NICKNAME,
-        SUM(B.PRICE) AS TOTAL_SALES
+        ANIMAL_TYPE,
+        NAME,
+        SEX_UPON_INTAKE
     FROM
-        USED_GOODS_USER U
-    JOIN
-        USED_GOODS_BOARD B
-        ON U.USER_ID = B.WRITER_ID
-    WHERE
-        B.STATUS = 'DONE'
-    GROUP BY
-        U.USER_ID,
-        U.NICKNAME
-    HAVING
-        SUM(B.PRICE) >= 700000
+        ANIMAL_INS
     ORDER BY
-        TOTAL_SALES ASC;
+        ANIMAL_TYPE;
 */
