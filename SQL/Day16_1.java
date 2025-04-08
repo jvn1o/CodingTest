906e08e8c6ae8d0e82b67915786709e7c7e9986d
jvn1o <junok0987@naver.com>
2025-04-08 21:30:55 +0900

    Day16

--- SQL/Day16_1.java
+++ SQL/Day16_1.java
@@ -20,5 +20,15 @@
 
 /*
     SELECT
-
+        CAR_ID,
+        ROUND(AVG(END_DATE - START_DATE + 1), 2) AS AVERAGE_DURATION
+    FROM
+        CAR_RENTAL_COMPANY_RENTAL_HISTORY
+    GROUP BY
+        CAR_ID
+    HAVING
+        AVG(END_DATE - START_DATE + 1) >= 7
+    ORDER BY
+        AVERAGE_DURATION DESC,
+        CAR_ID DESC;
 */
