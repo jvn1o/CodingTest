a7a71f70c8dffe52d1a92d669597609969cde8e8
jvn1o <junok0987@naver.com>
2025-04-15 00:00:13 +0900

    Day17

--- SQL/Day17_2.java
+++ SQL/Day17_2.java
@@ -36,16 +36,5 @@
 
 /*
     SELECT
-        '/home/grep/src/' || f.BOARD_ID || '/' || f.FILE_ID || f.FILE_NAME || '.' || f.FILE_EXT AS FILE_PATH
-    FROM USED_GOODS_FILE f
-    JOIN (
-        SELECT BOARD_ID
-        FROM (
-            SELECT BOARD_ID
-            FROM USED_GOODS_BOARD
-            ORDER BY VIEWS DESC
-        )
-        WHERE ROWNUM = 1
-    ) b ON f.BOARD_ID = b.BOARD_ID
-    ORDER BY f.FILE_ID DESC;
+        
 */
